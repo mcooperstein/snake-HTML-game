f920e3769c44b0f6934dd0c95dd77f92e12354bc
Marc Cooperstein <mcooperstein@gmail.com>
2016-09-29 21:06:07 -0700

    refactoring JS
    -refactoring JS file

--- app.js
+++ app.js
@@ -1,150 +1,96 @@
-$(document).ready(function () {
-    //Setting up the canvas
+var JS_Snake = {};
 
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
+JS_Snake.game = (function () {
+    var ctx;
+    //var xPos = 0;
+    //var yPos = 0;
+    JS_Snake.width = 500;
+    JS_Snake.height = 500;
+    JS_Snake.blockSize = 10;
+    var frameLength = 100; //new frame every 100ms
+    var snake;
 
-    var w = canvas.width;
-    var h = canvas.height;
-
-    var cw = 10 //cell width for snake and food
-        //var direction; //direction snake is moving
-    var rightPressed = true;
-    var leftPressed = false;
-    var downPressed = false;
-    var upPressed = false;
-    var food;
-    var score = 0;
-    //initializing the snake array
-    //an array of cells to make up the snake
-    var snake_array = [];
-
-    document.addEventListener("keydown", keyDownHandler, false);
-    document.addEventListener("keyup", keyUpHandler, false);
-
-    function keyDownHandler(e) {
-        if (e.keyCode == 39) {
-            rightPressed = true;
-        } else if (e.keyCode == 37) {
-            leftPressed = true;
-        } else if (e.keyCode == 38) {
-            upPressed = true;
-        } else if (e.keyCode == 40) {
-            downPressed = true;
-        }
+    function init() {
+        $('body').append('<canvas id="jsSnake">');
+        var $canvas = $("#jsSnake");
+        $canvas.attr("width", 500);
+        $canvas.attr("height", 500);
+        var canvas = $canvas[0];
+        ctx = canvas.getContext("2d");
+        snake = JS_Snake.snake();
+        bindEvents();
+        gameLoop();
     }
 
-    function keyUpHandler(e) {
-        if (e.keyCode == 39) {
-            rightPressed = false;
-        } else if (e.keyCode == 37) {
-            leftPressed = false;
-        } else if (e.keyCode == 38) {
-            upPressed = false;
-        } else if (e.keyCode == 40) {
-            downPressed = false;
-        }
+    function gameLoop() {
+        //xPos += 2;
+        //yPos += 4;
+        ctx.clearRect(0, 0, JS_Snake.width, JS_Snake.height);
+        snake.advance();
+        snake.draw(ctx);
+        setTimeout(gameLoop, frameLength);
     }
 
-    function draw_snake() {
-        var length = 5; //starting length of snake
-
-        for (var i = length - 1; i >= 0; i--) {
-            snake_array.push({
-                x: i,
-                y: 0
-            });
-            //this creates a snake starting with 5 cells
-        }
-    }
-
-    function draw_food() {
-        food = {
-            x: Math.round(Math.random() * (w - cw) / cw),
-            y: Math.round(Math.random() * (h - cw) / cw)
+    function bindEvents() {
+        var keysToDirections = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down'
         };
-        /*This will create a cell with x/y between 0-49, because
-        there are 50 (500/10) positions across the rows/columns*/
+        $(document).keydown(function (event) {
+            var key = event.which;
+            var direction = keysToDirections[key];
+
+
+        })
+
     }
 
-    function collision(x, y, array) {
-        /*This function will check if the provided x/y coordinates exist
-        in an array of cells or not*/
-        for (var i = 0; i < array.length; i++) {
-            if (array[i].x == x && array[i].y == y)
-                return true;
+    return {
+        init: init
+    };
+})();
+
+JS_Snake.snake = function () {
+    var posArray = [];
+    posArray.push([6, 4]);
+    posArray.push([5, 4]);
+    posArray.push([4, 4]);
+    var direction = 'right';
+
+    function set
+
+    function drawSection(ctx, position) {
+        var x = JS_Snake.blockSize * position[0];
+        var y = JS_Snake.blockSize * position[1];
+        ctx.fillRect(x, y, JS_Snake.blockSize, JS_Snake.blockSize);
+    }
+
+    function draw(ctx) {
+        ctx.save();
+        ctx.fillStyle = 'green';
+        for (var i = 0; i < posArray.length; i++) {
+            drawSection(ctx, posArray[i]);
         }
-        return false;
+        ctx.restore();
     }
 
-    function paint_cells(x, y) {
-        ctx.fillStyle = "blue";
-        ctx.fillRect(x * cw, y * cw, cw, cw);
-        ctx.strokeStyle = "white";
-        ctx.strokeRect(x * cw, y * cw, cw, cw);
+    function advance() {
+        var nextPosition = posArray[0].slice(); //copy head of snake
+        nextPosition[0] += 1; //add 1 to the x position
+        //add the new position to the beginning of the array
+        posArray.unshift(nextPosition);
+        //and remove the last position
+        posArray.pop();
     }
-
-    function draw_score() {
-        ctx.font = "16px Arial";
-        ctx.fillStyle = "blue";
-        ctx.fillText("Score: " + score, 8, 20);
-    }
-
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        draw_snake();
-        //draw_food();
-        //The movement code for the snake to come here.
-        //The logic is simple
-        //Pop out the tail cell and place it infront of the head cell
-        var nx = snake_array[0].x;
-        var ny = snake_array[0].y;
-        //These were the position of the head cell.
-        //We will increment it to get the new head position
-        //Lets add proper direction based movement now
-        if (rightPressed) nx++;
-        else if (leftPressed) nx--;
-        else if (upPressed) ny--;
-        else if (downPressed) ny++;
-
-        // If the snake hits the wall or bumps into its body, you lose and the game will restart
-        if (nx == -1 || nx == w / cw || ny == -1 || ny == h / cw || collision(nx, ny, snake_array)) {
-            //restart game
-            alert("Game Over, You Lose!");
-            document.location.reload();
-        }
-
-        /*The logic for the snake eating the food:
-        If the new head position matches with that of the food,
-        Create a new head instead of moving the tail*/
-        if (nx == food.x && ny == food.y) {
-            var tail = {
-                x: nx,
-                y: ny
-            };
-            score++;
-            //Create new food
-            draw_food();
-        } else {
-            var tail = snake_array.pop(); //pops out the last cell
-            tail.x = nx;
-            tail.y = ny;
-        }
-        //The snake can now eat the food.
-
-        snake_array.unshift(tail); //puts back the tail as the first cell
-
-        for (var i = 0; i < snake_array.length; i++) {
-            var c = snake_array[i];
-            //Lets paint 10px wide cells
-            paint_cells(c.x, c.y);
-        }
-
-        //Lets paint the food
-        paint_cells(food.x, food.y);
-        draw_score();
-    }
-    setInterval(draw, 60);
+    return {
+        draw: draw,
+        advance: advance
+    };
+};
 
 
+$(document).ready(function () {
+    JS_Snake.game.init();
 });
